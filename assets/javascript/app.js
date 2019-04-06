8961b85f863b69b4c4a88bc5e5301be4c4f264d4
ericktorrs1 <ericktorrs90.et@gmail.com>
2019-04-06 14:58:38 -0500

    updated project

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    var counter = 240;
+    
+
+    var timer = setInterval(function() {
+       $('#card-content').html("<h2> Time Remaining: " + --counter  +" </h2>");
+
+    // To do:When the counter reaches to zero 
+       if(counter <= 0){
+           $('#card-content').html("<h2> TIME UP! </h2>")
+       }
+
+        
+    }, 1000);
+    
+    var startButton = $('#startButton');
+    var cardContentDOM = $('#card-content');
+    startButton.on('click', function () {
+        $(startButton).remove();
+        cardContentDOM.html(time());
+
+        
+    
+    });
+});
+
+
