161369c1e3ad486ce4447a4b1f73c196d6dd2278
ericktorrs1 <ericktorrs90.et@gmail.com>
2019-04-06 15:04:10 -0500

    fixed bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,24 @@
+var counter = 240;
+var timer = setInterval(function() {
+    $('#card-content').html("<h2> Time Remaining: " + --counter  +" </h2>");
+
+ // To do:When the counter reaches to zero 
+    if(counter <= 0){
+        $('#card-content').html("<h2> TIME UP! </h2>");
+    }
+
+     
+ }, 1000);
+
 $(document).ready(function(){
 
-    var counter = 240;
     
 
-    var timer = setInterval(function() {
-       $('#card-content').html("<h2> Time Remaining: " + --counter  +" </h2>");
-
-    // To do:When the counter reaches to zero 
-       if(counter <= 0){
-           $('#card-content').html("<h2> TIME UP! </h2>")
-       }
-
-        
-    }, 1000);
-    
     var startButton = $('#startButton');
     var cardContentDOM = $('#card-content');
     startButton.on('click', function () {
         $(startButton).remove();
-        cardContentDOM.html(time());
+        cardContentDOM.html(timer());
 
         
     
